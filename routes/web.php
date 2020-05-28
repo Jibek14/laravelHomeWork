8874709af84509976d60e08ab31e53064d9f6547
Jibek14 <61323163+Jibek14@users.noreply.github.com>
2020-05-28 15:45:39 +0600

    Update web.php
    file edited

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/','TaskController@insertform');
+Route::get('/tasks', function () {
+   // $tasks=DB::table('tasks')->get();
+    $tasks=App\Task::all();
+    return view('tasks.index',compact('tasks'));
+});
+Route::get('/tasks/{task}', function ($id) {
+   // $task=DB::table('tasks')->find($id);
+    $task=App\Task::find($id);
+    return view('tasks.show',compact('task'));
+});
+Route::get('insert','TaskController@insertform');
 Route::post('create','TaskController@insert');
